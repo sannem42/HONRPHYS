2f32025eaaeae66617513e63b7876bf0e87aab43
NEWUSER <sannem42@umd.edu>
2017-10-01 21:27:10 -0400

    hw4 question1

--- hw4q1.cpp
+++ hw4q1.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main (){
-  int nfact = 1;
-  for (int n=1; n<=10; n++){
-    for (int i=1; i<=n; i++)
-      nfact = nfact*i;
+  int nfact = 1; //initialize n-factorial to 1
+  for (int n=1; n<=10; n++){ //ths for loop prints all the n's and their calculated factorial variables
+    for (int i=1; i<=n; i++)//this foor loop is responsible for calculating each factoral
+      nfact = nfact*i; //calculation of factorial
     cout << n << "! = " << nfact << endl;
-    nfact = 1;
+    nfact = 1; //reset nfact to 1 for next time through loop
   }
   return 0;
 }
